fd4aeed15271099b378db27e7d32b418eb9f5a66
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-06-22 21:25:48 -0300

    Update firebase file

--- have-questions/src/services/firebase.ts
+++ have-questions/src/services/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDgKHGiM2QWSVJuUJb5mmFeXig-uSsGfcE",
-    authDomain: "have-question-react.firebaseapp.com",
-    databaseURL: "https://have-question-react-default-rtdb.firebaseio.com",
-    projectId: "have-question-react",
-    storageBucket: "have-question-react.appspot.com",
-    messagingSenderId: "983011664764",
-    appId: "1:983011664764:web:2c65078551d9ab727f7af9"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
