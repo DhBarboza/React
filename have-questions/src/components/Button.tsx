10e220448a98f632f90718f2bf2561bab2f98b79
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-06-23 00:53:14 -0300

    Add styled in Home page

---
+++ have-questions/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { ButtonHTMLAttributes } from 'react';
+
+type ButtonProps = {
+    type: string;
+
+}
+
+export function Button(props: ButtonProps) {
+    return (
+        <button className="button" {...props}/>
+    )
+}
+
+<Button type="submit"/>
--- have-questions/src/pages/Home.tsx
+++ have-questions/src/pages/Home.tsx
@@ -1,9 +1,30 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIcon from '../assets/images/google-icon.svg';
+import '../styles/auth.scss';
+
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
-                
+                <img src={illustrationImg} alt="Ilustração de perguntas e respostas" />
+                <strong>Criar salas de Perguntas&amp;Respostas em tempo real</strong>
+                <p>Tire as duvidas dos seus ouvintes</p>
             </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="HaveQuestion" />
+                    <button className="create-room">
+                        <img src={googleIcon} alt="" />
+                        Crie sua sala com Google
+                    </button>
+                    <div className="separator">Ou entre em uma sala</div>
+                    <form >
+                        <input type="text" placeholder="Digite o código da sala"/>
+                        <button type="submit">Entrar na sala</button>
+                    </form>
+                </div>
+            </main>
         </div>
     )
 }
